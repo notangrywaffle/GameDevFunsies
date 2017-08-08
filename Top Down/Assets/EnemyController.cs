12cb2bf22813fd1e7022217b6eb1ecb8f1e33322
LametownBandit <marisagloyne@gmail.com>
2017-08-08 22:22:21 +1200

    Add EnemyController.cs Start attack framework

---
+++ Top Down/Assets/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public GameObject Enemy;
+
+    //Enemy stats
+    private int attack = 1;
+    private int health = 1;
+    private int hitRange = 1;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TakeDamage(int damageAmount)
+    {
+        health = health - damageAmount;
+
+        if (health < 0)
+        {
+            // This enemy is supposed to be dead now.
+            Enemy.SetActive(false);
+        }
+    }
+}
--- Top Down/Assets/Prototype/PlayerController.cs
+++ Top Down/Assets/Prototype/PlayerController.cs
@@ -3,6 +3,11 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour
 {
+    //Player stats
+    private int attack = 100;
+    private int health = 10;
+    private int hitRange = 1000;
+
 
     void Start()
     {
@@ -15,15 +20,43 @@ public class PlayerController : NetworkBehaviour
 
     void Update()
     {
+
+        //// Check player instance 
         //if (!isLocalPlayer)
         //{
         //    return;
         //}
 
+        // Check for player movement
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
         transform.Rotate(0, 0, -x);
         transform.Translate(0, z, 0);
+
+        //Check for player attack
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Attack();
+        }
     }
+
+    void Attack()
+    {
+        Debug.Log("AttemptAttack");
+
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.up);
+        Vector3 origin = transform.position;
+
+        if (Physics.Raycast(origin, forward, out hit, hitRange))
+        {
+            if (hit.transform.gameObject != null && hit.transform.tag == "Enemy")
+            {
+                Debug.Log("Hit!");
+                hit.transform.gameObject.SendMessage("TakeDamage", 30);
+            }
+        }
+    }
+    
 }
