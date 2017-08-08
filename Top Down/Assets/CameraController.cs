a26267198f56cd8b6cc95f1fe0715120001603b6
LametownBandit <marisagloyne@gmail.com>
2017-08-08 20:31:48 +1200

    lerp added to camera

--- Top Down/Assets/CameraController.cs
+++ Top Down/Assets/CameraController.cs
@@ -6,21 +6,26 @@ public class CameraController : MonoBehaviour {
 
     public GameObject Player;       //Public variable to store a reference to the player game object
 
-
     private Vector3 offset;         //Private variable to store the offset distance between the player and camera
 
-    // Use this for initialization
-    void Start()
-    {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - Player.transform.position;
-    }
+    public float followRate = 5f;
+    float followHeight = -20f;
 
-    // LateUpdate is called after Update each frame
     void LateUpdate()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = Player.transform.position + offset;
+
+        Vector3 startPos = transform.position;
+
+        startPos.z = followHeight;
+
+        Vector3 endPos = Player.transform.position;
+
+        endPos.z = followHeight;
+
+        transform.position = Vector3.Lerp(startPos, endPos, Time.deltaTime * followRate);
+
     }
+
 }
 
+     
