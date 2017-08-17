4c8d632f9256219a6bfa1b91425a976b07c106a8
Liam <l.pool@live.com>
2017-08-17 21:11:28 +1200

    Configured network options thing...?

---
+++ Top Down/Assets/Prototype/LanManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LanManager : NetworkManager {
+
+	public new string networkAddress = "localhost";
+
+	//// Use this for initialization
+	//void Start () {
+ //       NetworkManager man = new NetworkManager();
+ //       man.networkAddress
+	//}
+	
+	//// Update is called once per frame
+	//void Update () {
+		
+	//}
+}
